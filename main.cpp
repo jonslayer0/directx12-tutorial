689b89fe14c97db1d8d9155968f047e9225ed80f
jonslayer0 <jonslayer0>
2025-06-30 12:52:34 +0200

    Prepare Window And Chosen GPU Add link to d3d12 librairies in projects Add Dll for shader compilation at runtime

--- main.cpp
+++ main.cpp
@@ -38,31 +38,185 @@ using namespace Microsoft::WRL;
 #include <Helpers.h>
 
 // The number of swap chain back buffers.
-const uint8_t g_NumFrames = 3;
+const uint8_t g_numFrames = 3;
 
 // Use WARP adapter
-bool g_UseWarp = false;
-uint32_t g_ClientWidth = 1280;
-uint32_t g_ClientHeight = 720;
+bool g_useWarp = false;
+uint32_t g_clientWidth = 1280;
+uint32_t g_clientHeight = 720;
 
 // Set to true once the DX12 objects have been initialized.
-bool g_IsInitialized = false;
+bool g_isInitialized = false;
 
 // Window handle.
 HWND g_hWnd;
-RECT g_WindowRect;
+RECT g_windowRect;
 
 // DirectX 12 Objects
-ComPtr<ID3D12Device2>				g_Device;
-ComPtr<ID3D12CommandQueue>			g_CommandQueue;
-ComPtr<IDXGISwapChain4>				g_SwapChain;
-ComPtr<ID3D12Resource>				g_BackBuffers[g_NumFrames];
-ComPtr<ID3D12GraphicsCommandList>	g_CommandList;
-ComPtr<ID3D12CommandAllocator>		g_CommandAllocators[g_NumFrames];
-ComPtr<ID3D12DescriptorHeap>		g_RTVDescriptorHeap;
+ComPtr<ID3D12Device2>				g_device;
+ComPtr<ID3D12CommandQueue>			g_commandQueue;
+ComPtr<IDXGISwapChain4>				g_swapChain;
+ComPtr<ID3D12Resource>				g_backBuffers[g_numFrames];
+ComPtr<ID3D12GraphicsCommandList>	g_commandList;
+ComPtr<ID3D12CommandAllocator>		g_commandAllocators[g_numFrames];
+ComPtr<ID3D12DescriptorHeap>		g_rtvDescriptorHeap;
+UINT								g_rtvDescriptorSize = 0u;
+UINT								g_currentBackBufferIndex = 0u;
 
-UINT g_RTVDescriptorSize = 0u;
-UINT g_CurrentBackBufferIndex = 0u;
+// Synchronization objects
+ComPtr<ID3D12Fence> g_fence;
+uint64_t	g_fenceValue = 0;
+uint64_t	g_frameFenceValues[g_numFrames] = {};
+HANDLE		g_fenceEvent;
+
+// By default, enable V - Sync (V key)
+bool g_vsync = true;
+bool g_tearingSupported = false;
+
+// By default, use windowed mode (Alt+Enter or F11)
+bool g_fullscreen = false;
+
+// Window callback function.
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void ParseCommandLineArguments()
+{
+    int argc;
+    wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+    for (size_t i = 0; i < argc; ++i)
+    {
+        if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0)
+        {
+            g_clientWidth = ::wcstol(argv[++i], nullptr, 10);
+        }
+
+        if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0)
+        {
+            g_clientHeight = ::wcstol(argv[++i], nullptr, 10);
+        }
+
+        if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0)
+        {
+            g_useWarp = true;
+        }
+    }
+
+    // Free memory allocated by CommandLineToArgvW
+    ::LocalFree(argv);
+}
+
+void EnableDebugLayer()
+{
+#if defined(_DEBUG)
+    // Always enable the debug layer before doing anything DX12 related
+    // so all possible errors generated while creating DX12 objects
+    // are caught by the debug layer.
+    ComPtr<ID3D12Debug> debugInterface;
+    ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+    debugInterface->EnableDebugLayer();
+#endif
+}
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+    // Register a window class for creating our render window with.
+    WNDCLASSEXW windowClass = {};
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = &WndProc;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = hInst;
+    windowClass.hIcon = ::LoadIcon(hInst, NULL);
+    windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+    windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    windowClass.lpszMenuName = NULL;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+
+    assert(::RegisterClassExW(&windowClass) > 0);
+}
+
+HWND CreateWindow(const wchar_t* windowClassName, HINSTANCE hInst,
+    const wchar_t* windowTitle, uint32_t width, uint32_t height)
+{
+    int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+    RECT windowRect = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    // Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+    HWND hWnd = ::CreateWindowExW(
+        NULL,
+        windowClassName,
+        windowTitle,
+        WS_OVERLAPPEDWINDOW,
+        windowX,
+        windowY,
+        windowWidth,
+        windowHeight,
+        NULL,
+        NULL,
+        hInst,
+        nullptr
+    );
+
+    assert(hWnd && "Failed to create window");
+    return hWnd;
+}
+
+
+ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp)
+{
+    ComPtr<IDXGIFactory4> dxgiFactory;
+    UINT createFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+    ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory)));
+
+    ComPtr<IDXGIAdapter1> dxgiAdapterTemp;  // Use to find best adapter
+    ComPtr<IDXGIAdapter4> dxgiAdapterFinal; // Returned best adapter
+
+    if (useWarp)
+    {
+        ThrowIfFailed(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapterTemp)));
+        ThrowIfFailed(dxgiAdapterTemp.As(&dxgiAdapterFinal));
+    }
+    else
+    {
+        SIZE_T maxDedicatedVideoMemory = 0;
+
+        for (UINT i = 0; dxgiFactory->EnumAdapters1(i, &dxgiAdapterTemp) != DXGI_ERROR_NOT_FOUND; ++i)
+        {
+            DXGI_ADAPTER_DESC1 dxgiAdapterDesc1;
+            dxgiAdapterTemp->GetDesc1(&dxgiAdapterDesc1);
+
+            // Check to see if the adapter can create a D3D12 device without actually 
+            // creating it. The adapter with the largest dedicated video memory is favored.
+
+            if ((dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0 &&
+                SUCCEEDED(D3D12CreateDevice(dxgiAdapterTemp.Get(),D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+                dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory)
+            {
+                maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+                ThrowIfFailed(dxgiAdapterTemp.As(&dxgiAdapterFinal));
+            }
+        }
+    }
+
+    return dxgiAdapterFinal;
+}
 
 int main()
 {
