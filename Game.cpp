d4a78527d54a1b42c06405fcf53aca1877988092
jonslayer0 <jonsiret@gmail.com>
2025-07-16 21:31:21 +0200

    Game empty functions (test commit from other PC)

---
+++ Game.cpp
@@ -0,0 +1,76 @@
+#include "Game.h"
+
+GAME::GAME(string name, int width, int height, bool vSync)
+{
+
+}
+
+GAME::~GAME() 
+{
+
+}
+
+bool GAME::Intitialize()
+{
+
+}
+
+bool GAME::LoadContent()
+{
+
+}
+
+void GAME::UnloadContent()
+{
+
+}
+
+void GAME::Destroy()
+{
+
+}
+
+void GAME::OnUpdate()
+{
+
+}
+
+void GAME::OnRender()
+{
+
+}
+
+void GAME::OnKeyPressed()
+{
+
+}
+
+void GAME::OnKeyReleased()
+{
+
+}
+
+void GAME::OnMouseMoved()
+{
+
+}
+
+void GAME::OnMouseButtonPressed()
+{
+
+}
+
+void GAME::OnMouseWheel()
+{
+
+}
+
+void GAME::OnResize()
+{
+
+}
+
+void GAME::OnWindowDestroy()
+{
+
+}
